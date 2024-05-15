45622e6daaa9662b235717a0ebabf08214e5396a
luq <luqman.yazrie@outlook.com>
2024-05-15 15:18:14 +0800

    69 loop

--- script.asm
+++ script.asm
@@ -5,8 +5,11 @@ section .bss
 
 section .data
     ;contants
-    hello: db "Heyy, Mommy..!, 10      ; string to print, by itself does nothing
+    hello: db "Heyy, Mommy..!", 10      ; string to print, by itself does nothing
     helloLen: equ $-hello       ; length of string
+
+    congrats: db "Mommy look, I counted till 69!", 10
+    congratsLen: equ $-congrats
 
 section .text
     global _start        ; entry level for code
@@ -17,11 +20,25 @@ section .text
         mov rdi, 1      ; standard out
         mov rsi, hello      ; message to write
         mov rdx, helloLen       ; message length
-        syscall     ; call kernel
+        syscall     ; call kernel (execute)
 
         ; each program needs to end otherwhise we'll get segmentation fault
+
+        loop:       ; loop counts till 69
+            cmp rax, 69     ; terminates after 69 cycles
+            je end_loop     ; go to end loop
+
+            inc rax     ; increments and repeat
+            jmp loop        ; jump back to top
+
+        end_loop:       ; when finish loop
+            mov rax, 1      ; system write
+            mov rdi, 1      ; standard out
+            mov rsi, congrats   ; write congrats
+            mov rdx, congratsLen        ; congrats message length
+            syscall     ; call kernel (execute)
 
         ; end program
         mov rax, 60     ; system exit
         mov rdi,0       ; 0 error code (success)
-        syscall
+        syscall     ; call kernel (execute)
