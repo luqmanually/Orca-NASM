a892ef111fe4ca0f1f9639d8c8399567ee637da5
luq <luqman.yazrie@outlook.com>
2024-05-16 14:29:04 +0800

    works?

--- script.asm
+++ script.asm
@@ -1,44 +1,93 @@
-; x86 Assembly
-
-section .bss
-    ;variables
-
 section .data
-    ;contants
-    hello: db "Heyy, Mommy..!", 10      ; string to print, by itself does nothing
-    helloLen: equ $-hello       ; length of string
-
-    congrats: db "Mommy look, I counted till 69!", 10
-    congratsLen: equ $-congrats
+    count: DB 0
+    welcomeMsg: DD "Orca-NASM", 10
+    welcomeMsgLen: equ $-welcomeMsg
+    finishMsg: DD "Finished counting", 10
+    finishMsgLen: equ $-finishMsg
+    buffer: times 4 db 0  ; Reserve space for a 4-character string (2 for cycle number, 2 for value)
 
 section .text
-    global _start        ; entry level for code
+global _start
 
-    _start:     ; program starts here
-        ;instructions       ;operand
-        mov rax, 1      ; system write
-        mov rdi, 1      ; standard out
-        mov rsi, hello      ; message to write
-        mov rdx, helloLen       ; message length
-        syscall     ; call kernel (execute)
+    _start:
+        ; Print out welcome message
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, welcomeMsg
+        mov rdx, welcomeMsgLen
+        syscall
 
-        ; each program needs to end otherwhise we'll get segmentation fault
+        mov rbx, 1  ; Value to print
+        mov rcx, 1  ; Cycle counter
+        ; Begin looping
+        loop:
+            ; If cycle number is 70, exit loop
+            cmp rcx, 70
+            je end_loop
 
-        loop:       ; loop counts till 69
-            cmp rax, 69     ; terminates after 69 cycles
-            je end_loop     ; go to end loop
+            ; Convert cycle number to string and store in buffer
+            mov rax, rcx
+            mov rdi, buffer
+            call convert_to_string
 
-            inc rax     ; increments and repeat
-            jmp loop        ; jump back to top
+            ; Print cycle number
+            mov rax, 1
+            mov rdi, 1
+            mov rsi, buffer
+            mov rdx, 2  ; Print 2 bytes (2-digit cycle number)
+            syscall
 
-        end_loop:       ; when finish loop
-            mov rax, 1      ; system write
-            mov rdi, 1      ; standard out
-            mov rsi, congrats   ; write congrats
-            mov rdx, congratsLen        ; congrats message length
-            syscall     ; call kernel (execute)
+            ; Print separator
+            mov rax, 1
+            mov rdi, 1
+            mov rsi, separator
+            mov rdx, 1
+            syscall
 
+            ; Convert value to string and store in buffer
+            mov rax, rbx
+            mov rdi, buffer
+            call convert_to_string
+
+            ; Print out the value of rbx
+            mov rax, 1
+            mov rdi, 1
+            mov rsi, buffer
+            mov rdx, 2  ; Print 2 bytes (2-digit value)
+            syscall
+
+            ; Print newline
+            mov rax, 1
+            mov rdi, 1
+            mov rsi, newline
+            mov rdx, 1
+            syscall
+
+            ; Increment rbx and rcx
+            inc rbx
+            inc rcx
+            jmp loop
+
+        end_loop:
+            ; Print out finish
+            mov rax, 1
+            mov rdi, 1
+            mov rsi, finishMsg
+            mov rdx, finishMsgLen
+            syscall
+        
         ; end program
-        mov rax, 60     ; system exit
-        mov rdi,0       ; 0 error code (success)
-        syscall     ; call kernel (execute)
+        mov rax, 60
+        mov rdi, 0
+        syscall
+
+    convert_to_string:
+        ; Convert value in rax to a 2-digit string in rdi
+        ; (Assuming rax is a single-digit number)
+        add rax, '0'  ; Convert to ASCII
+        mov [rdi], al
+        ret
+
+    section .data
+        newline: db 10, 0
+        separator: db ':', 0
