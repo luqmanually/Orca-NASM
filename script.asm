cc2aff54bd9a92e92f6ca1930ea97dd325b1e18e
luq <luqman.yazrie@outlook.com>
2024-05-18 17:03:45 +0800

    _console_out works as intended

--- script.asm
+++ script.asm
@@ -1,50 +1,69 @@
 section .data
-    message_intro: dd "Organic NASM based project - Orca-NASM", 10
-    message_commands: dd "<calc> to use calculator.", 10, "<exit> to exit.", 10
+    message_intro: dd "Organic NASM-based project - Orca-NASM"
+    message_commands: dd "<calc> to use calculator. | <exit> to exit."
     message_yes: dd "Yes", 10
+
+    newline: db 10 ; Newline
 
 section .bss
     command: resb 16
     operator: resb 8
     firstNum: resb 8
     secondNum: resb 8
-    message: resb 64
+    messageLen: resb 64
 
 section .text
     global _start
 
 _start:
-    mov rbx, message_intro ; send intro message
+    ; start of code
+    mov rax, message_intro
+    call _console_out
+
+    mov rax, message_commands
     call _console_out
 
     call _exit ; exit
 
 _console_out:
-    mov rax, 13
-    mov rdi, rbx
-    syscall
-
-    mov rax, rcx
-    
-    ; subroutine that prints out a message
+    ; subroutine to print out the message stored in rax
+    push rax
+    mov rbx, 0
+    loop:
+        ; increase pointer for the message and count
+        inc rax
+        inc rbx
+        mov cl, [rax] ; load byte to memory address
+        cmp cl, 0 ; check for null terminator
+    jne loop
+ 
+    ; print out message
     mov rax, 1
     mov rdi, 1
-    mov rsi, rbx ; rbx needs to contain the message string
-    mov rdx, rcx
+    pop rsi ; retrive original memory address from stack into rsi
+    mov rdx, rbx ; length of message to print
     syscall
-    ret
+ 
+    ; print out new line
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    ret ; return to code/end of subroutine
 
 _get:
     ; subroutine that fetches input
     mov rax, 0
     mov rdi, 0
-    mov rsi, rbx ; rbx is where the input will be stored
+    mov rsi, rax ; rbx is where the input will be stored
     mov rdx, 256 ; 256 bytes allocation
     syscall
     ret
 
 _exit:
-    ; subroutine to quit code
+    ; subroutine to quit code. required, otherwise segmentation fault.
     mov rax, 60
     mov rdi, 0
     syscall
