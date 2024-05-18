3e7ea459cb0a72838333208daabd8e512eee5e7d
luq <luqman.yazrie@outlook.com>
2024-05-18 16:21:29 +0800

    output works, but unintended output

--- script.asm
+++ script.asm
@@ -1,6 +1,7 @@
 section .data
-    message_intro: db "Organic NASM-based project - Orca-NASM", 10
-    message_commands: db "<calc> to use calculator.", 10, "<exit> to exit.", 10
+    message_intro: dd "Organic NASM based project - Orca-NASM", 10
+    message_commands: dd "<calc> to use calculator.", 10, "<exit> to exit.", 10
+    message_yes: dd "Yes", 10
 
 section .bss
     command: resb 16
@@ -13,21 +14,33 @@ section .text
     global _start
 
 _start:
-    mov rbx, message_intro
-
-    ; Call _console_out with the dynamic length
+    mov rbx, message_intro ; send intro message
     call _console_out
 
-    call _exit
+    call _exit ; exit
 
 _console_out:
+    mov rax, 13
+    mov rdi, rbx
+    syscall
+
+    mov rax, rcx
+    
     ; subroutine that prints out a message
     mov rax, 1
     mov rdi, 1
-    mov rsi, rbx
-    mov rdx, 256
+    mov rsi, rbx ; rbx needs to contain the message string
+    mov rdx, rcx
     syscall
+    ret
 
+_get:
+    ; subroutine that fetches input
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, rbx ; rbx is where the input will be stored
+    mov rdx, 256 ; 256 bytes allocation
+    syscall
     ret
 
 _exit:
@@ -35,5 +48,4 @@ _exit:
     mov rax, 60
     mov rdi, 0
     syscall
-
     ret
