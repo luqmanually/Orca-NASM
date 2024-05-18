c3885dbc5a20bb32f06765bd49a4c3a335f4a566
luq <luqman.yazrie@outlook.com>
2024-05-18 15:44:31 +0800

    working _console_out subroutine

--- script.asm
+++ script.asm
@@ -1,93 +1,39 @@
 section .data
-    count: DB 0
-    welcomeMsg: DD "Orca-NASM", 10
-    welcomeMsgLen: equ $-welcomeMsg
-    finishMsg: DD "Finished counting", 10
-    finishMsgLen: equ $-finishMsg
-    buffer: times 4 db 0  ; Reserve space for a 4-character string (2 for cycle number, 2 for value)
+    message_intro: db "Organic NASM-based project - Orca-NASM", 10
+    message_commands: db "<calc> to use calculator.", 10, "<exit> to exit.", 10
+
+section .bss
+    command: resb 16
+    operator: resb 8
+    firstNum: resb 8
+    secondNum: resb 8
+    message: resb 64
 
 section .text
-global _start
+    global _start
 
-    _start:
-        ; Print out welcome message
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, welcomeMsg
-        mov rdx, welcomeMsgLen
-        syscall
+_start:
+    mov rbx, message_intro
 
-        mov rbx, 1  ; Value to print
-        mov rcx, 1  ; Cycle counter
-        ; Begin looping
-        loop:
-            ; If cycle number is 70, exit loop
-            cmp rcx, 70
-            je end_loop
+    ; Call _console_out with the dynamic length
+    call _console_out
 
-            ; Convert cycle number to string and store in buffer
-            mov rax, rcx
-            mov rdi, buffer
-            call convert_to_string
+    call _exit
 
-            ; Print cycle number
-            mov rax, 1
-            mov rdi, 1
-            mov rsi, buffer
-            mov rdx, 2  ; Print 2 bytes (2-digit cycle number)
-            syscall
+_console_out:
+    ; subroutine that prints out a message
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, rbx
+    mov rdx, 256
+    syscall
 
-            ; Print separator
-            mov rax, 1
-            mov rdi, 1
-            mov rsi, separator
-            mov rdx, 1
-            syscall
+    ret
 
-            ; Convert value to string and store in buffer
-            mov rax, rbx
-            mov rdi, buffer
-            call convert_to_string
+_exit:
+    ; subroutine to quit code
+    mov rax, 60
+    mov rdi, 0
+    syscall
 
-            ; Print out the value of rbx
-            mov rax, 1
-            mov rdi, 1
-            mov rsi, buffer
-            mov rdx, 2  ; Print 2 bytes (2-digit value)
-            syscall
-
-            ; Print newline
-            mov rax, 1
-            mov rdi, 1
-            mov rsi, newline
-            mov rdx, 1
-            syscall
-
-            ; Increment rbx and rcx
-            inc rbx
-            inc rcx
-            jmp loop
-
-        end_loop:
-            ; Print out finish
-            mov rax, 1
-            mov rdi, 1
-            mov rsi, finishMsg
-            mov rdx, finishMsgLen
-            syscall
-        
-        ; end program
-        mov rax, 60
-        mov rdi, 0
-        syscall
-
-    convert_to_string:
-        ; Convert value in rax to a 2-digit string in rdi
-        ; (Assuming rax is a single-digit number)
-        add rax, '0'  ; Convert to ASCII
-        mov [rdi], al
-        ret
-
-    section .data
-        newline: db 10, 0
-        separator: db ':', 0
+    ret
